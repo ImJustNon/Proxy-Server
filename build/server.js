5c5b99fbab5d54d98991bfdb17ba308e30eb602c
ImJustNon <Im_Just_Non@hotmail.com>
2024-12-23 18:07:16 +0700

    Git init

---
+++ build/server.js
@@ -0,0 +1,83 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const net_1 = __importDefault(require("net"));
+const dgram_1 = __importDefault(require("dgram"));
+const http_1 = __importDefault(require("http"));
+const http_proxy_1 = __importDefault(require("http-proxy"));
+const mappings = [
+    {
+        name: "Remote Desktop",
+        protocols: ['tcp', "udp"],
+        host: "nonandchain.thddns.net",
+        listenPort: 3389,
+        targetPort: 4848
+    },
+    {
+        name: "Remote SSH",
+        protocols: ['tcp', "udp"],
+        host: "127.0.0.1",
+        listenPort: 22,
+        targetPort: 4847
+    },
+];
+function createHttpProxy(name, host, listenPort, targetPort) {
+    const proxy = http_proxy_1.default.createProxyServer({});
+    const server = http_1.default.createServer((req, res) => {
+        proxy.web(req, res, {
+            target: `http://${host}:${targetPort}`
+        }, (err) => {
+            console.error(`HTTP proxy error for port ${listenPort}:`, err);
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Internal Server Error');
+        });
+    });
+    server.listen(listenPort, () => {
+        console.log(`[${name}] HTTP proxy listening on port ${listenPort}, forwarding to ${targetPort}`);
+    });
+}
+function createTcpProxy(name, host, listenPort, targetPort) {
+    const tcpServer = net_1.default.createServer((clientSocket) => {
+        const forwardSocket = net_1.default.createConnection(targetPort, host, () => {
+            console.log(`[${name}] TCP connection forwarded: ${listenPort} -> ${targetPort}`);
+        });
+        clientSocket.pipe(forwardSocket);
+        forwardSocket.pipe(clientSocket);
+        clientSocket.on('end', () => forwardSocket.end());
+        forwardSocket.on('end', () => clientSocket.end());
+        clientSocket.on('error', (err) => console.error(`TCP error on port ${listenPort}:`, err));
+        forwardSocket.on('error', (err) => console.error(`TCP forward error on port ${targetPort}:`, err));
+    });
+    tcpServer.listen(listenPort, () => {
+        console.log(`[${name}] TCP server listening on port ${listenPort}, forwarding to ${targetPort}`);
+    });
+}
+function createUdpProxy(name, host, listenPort, targetPort) {
+    const udpServer = dgram_1.default.createSocket('udp4');
+    udpServer.on('message', (msg, rinfo) => {
+        console.log(`[${name}] UDP message received on port ${listenPort}, forwarding to ${targetPort}`);
+        udpServer.send(msg, 0, msg.length, targetPort, host, (err) => {
+            if (err)
+                console.error(`UDP forward error: ${err}`);
+        });
+    });
+    udpServer.bind(listenPort, () => {
+        console.log(`[${name}] UDP server listening on port ${listenPort}, forwarding to ${targetPort}`);
+    });
+}
+mappings.forEach(({ name, protocols, host, listenPort, targetPort }) => {
+    protocols.forEach((proto) => {
+        if (proto === 'tcp') {
+            createTcpProxy(name, host, listenPort, targetPort);
+        }
+        else if (proto === 'udp') {
+            createUdpProxy(name, host, listenPort, targetPort);
+        }
+        else if (proto === 'http') {
+            createHttpProxy(name, host, listenPort, targetPort);
+        }
+    });
+});
+//# sourceMappingURL=server.js.map
---
+++ server.ts
@@ -0,0 +1,97 @@
+import net from "net";
+import dgram from "dgram";
+import http, { IncomingMessage, RequestListener, Server, ServerResponse } from "http";
+import httpProxy from "http-proxy";
+
+// Define port mappings for TCP, UDP, and HTTP
+const mappings = [
+    {
+        name: "Remote Desktop",
+        protocols: ['tcp', "udp"], 
+        host: "nonandchain.thddns.net",
+        listenPort: 3389, 
+        targetPort: 4848 
+    },
+    { 
+        name: "Remote SSH",
+        protocols: ['tcp', "udp"], 
+        host: "127.0.0.1",
+        listenPort: 22, 
+        targetPort: 4847 
+    },
+];
+
+// Function to handle HTTP proxying with http-proxy
+function createHttpProxy(name: string, host: string, listenPort: number, targetPort: number): void {
+    const proxy: httpProxy = httpProxy.createProxyServer({});
+
+    const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
+        proxy.web(req, res, { 
+            target: `http://${host}:${targetPort}` 
+        }, (err) => {
+            console.error(`HTTP proxy error for port ${listenPort}:`, err);
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Internal Server Error');
+        });
+    });
+
+    server.listen(listenPort, (): void => {
+        console.log(`[${name}] HTTP proxy listening on port ${listenPort}, forwarding to ${targetPort}`);
+    });
+}
+
+// Function to handle TCP proxying with net
+function createTcpProxy(name: string, host: string, listenPort: number, targetPort: number): void {
+    // Handle TCP traffic
+    const tcpServer: net.Server = net.createServer((clientSocket: net.Socket): void => {
+        const forwardSocket: net.Socket = net.createConnection(targetPort, host, (): void => {
+            console.log(`[${name}] TCP connection forwarded: ${listenPort} -> ${targetPort}`);
+        });
+
+        clientSocket.pipe(forwardSocket);
+        forwardSocket.pipe(clientSocket);
+
+        clientSocket.on('end', () => forwardSocket.end());
+        forwardSocket.on('end', () => clientSocket.end());
+
+        clientSocket.on('error', (err) => console.error(`TCP error on port ${listenPort}:`, err));
+        forwardSocket.on('error', (err) => console.error(`TCP forward error on port ${targetPort}:`, err));
+    });
+
+    tcpServer.listen(listenPort, () => {
+        console.log(`[${name}] TCP server listening on port ${listenPort}, forwarding to ${targetPort}`);
+    });
+}
+
+// Function to handle UDP proxying with dgram
+function createUdpProxy(name: string, host: string, listenPort: number, targetPort: number): void {
+    // Handle UDP traffic
+    const udpServer: dgram.Socket = dgram.createSocket('udp4');
+
+    udpServer.on('message', (msg: Buffer<ArrayBufferLike>, rinfo: dgram.RemoteInfo) => {
+
+        console.log(`[${name}] UDP message received on port ${listenPort}, forwarding to ${targetPort}`);
+
+        udpServer.send(msg, 0, msg.length, targetPort, host, (err: Error | null) => {
+            if (err) console.error(`UDP forward error: ${err}`);
+        });
+    });
+
+    udpServer.bind(listenPort, (): void => {
+        console.log(`[${name}] UDP server listening on port ${listenPort}, forwarding to ${targetPort}`);
+    });
+}
+
+// Start servers for each protocol based on mappings
+mappings.forEach(({ name, protocols, host, listenPort, targetPort }: { name: string; protocols: string[]; host: string; listenPort: number, targetPort: number }): void => {
+    protocols.forEach((proto: string): void => {
+        if (proto === 'tcp') {
+            createTcpProxy(name, host, listenPort, targetPort);
+        } else if (proto === 'udp') {
+            createUdpProxy(name, host, listenPort, targetPort);
+        } else if (proto === 'http') {
+            // Handle HTTP traffic
+            createHttpProxy(name, host, listenPort, targetPort);
+        }
+    });
+});
